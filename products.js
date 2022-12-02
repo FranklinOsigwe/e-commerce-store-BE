00f94129bca8cd74c483e68784d28b4c9a15a080
Franklin Osigwe <franklindutch1@gmail.com>
2022-12-02 19:57:40 +0100

    backend-store

---
+++ products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name: "Iphone 12 pro",
+        desc: "6.1-inch display",
+        price: 1109,
+        image: "https://res.cloudinary.com/ds6jcsjbl/image/upload/v1669824097/onlineShop/iphone14_tw5sg8.jpg"
+    },
+    {
+        id: 2,
+        name: "Iphone Galaxy x",
+        desc: "6.1-inch display",
+        price: 929,
+        image: "https://res.cloudinary.com/ds6jcsjbl/image/upload/v1669824096/onlineShop/Apple_iPhone_12_Pro_pqrrzq.jpg"
+    },
+    {
+        id: 3,
+        name: "Iphone 11 Lite",
+        desc: "6.1-inch display",
+        price: 999,
+        image: "https://res.cloudinary.com/ds6jcsjbl/image/upload/v1669824096/onlineShop/apple-iphone-13-pro-price-in-pakistan-4_0c7570e4-2334-4a5f-b937-a5cfb672b34a_420x_mmlwu7.webp"
+    },
+]
+
+module.exports = products;
